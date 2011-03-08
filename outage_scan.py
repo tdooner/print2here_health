b7181a6e14b5593f53b3d2ddf1f6603cb7e777d8
Alex Kloss <klossalex@gmail.com>
2011-03-08 14:19:56 -0500

    Wrap code in a function so I can test helper functions without running the script

--- outage_scan.py
+++ outage_scan.py
@@ -34,43 +34,46 @@ def seconds_to_string(seconds):
         string += "1 second  "
     return string[:-2]
 
+def main():
+    db = database.HealthDatabase('polling.db')
 
-db = database.HealthDatabase('polling.db')
+    polling_start = db.get_db_start() 
+    polling_end = db.get_db_end()
+    polling_length = round((polling_end - polling_start).total_seconds())
 
-polling_start = db.get_db_start() 
-polling_end = db.get_db_end()
-polling_length = round((polling_end - polling_start).total_seconds())
+    output = open(settings.REPORT_PAGE_PATH, 'w')
 
-output = open(settings.REPORT_PAGE_PATH, 'w')
+    output.write("""<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Print2Here Reliability Statistics</title>
+    </head>
+    <body>
+    <center>
+    <table border="1">
+      <tr>
+        <td>Printer</td>
+        <td>Percent Uptime</td>
+        <td>Average Outage Length</td>
+      </tr>""")
 
-output.write("""<!DOCTYPE html>
-<html>
-<head>
-  <title>Print2Here Reliability Statistics</title>
-</head>
-<body>
-<center>
-<table border="1">
-  <tr>
-    <td>Printer</td>
-    <td>Percent Uptime</td>
-    <td>Average Outage Length</td>
-  </tr>""")
+    for printer in settings.PRINTERS:
+        downtime = db.get_downtime(printer) 
+        avg_downtime = db.get_average_downtime(printer)
+        percent = round(((1 - (downtime / polling_length)) * 100), 2)
+        output.write("""
+      <tr>
+        <td>%s</td>
+        <td>%s%%</td>
+        <td>%s</td>
+      </tr>""" % (printer, percent, seconds_to_string(avg_downtime)))
 
-for printer in settings.PRINTERS:
-    downtime = db.get_downtime(printer) 
-    avg_downtime = db.get_average_downtime(printer)
-    percent = round(((1 - (downtime / polling_length)) * 100), 2)
     output.write("""
-  <tr>
-    <td>%s</td>
-    <td>%s%%</td>
-    <td>%s</td>
-  </tr>""" % (printer, percent, seconds_to_string(avg_downtime)))
+    </table>
+    </center>
+    Statistics gathered from %s to %s
+    </body>
+    </html>""" % (polling_start.strftime('%Y-%m-%d %H:%M:%S'), polling_end.strftime('%Y-%m-%d %H:%M:%S')))
 
-output.write("""
-</table>
-</center>
-Statistics gathered from %s to %s
-</body>
-</html>""" % (polling_start.strftime('%Y-%m-%d %H:%M:%S'), polling_end.strftime('%Y-%m-%d %H:%M:%S')))
+if __name__ == "__main__":
+    main()
