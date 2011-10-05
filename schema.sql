36d01c899af3c8e6e9a5a3f38cb024191648c4d8
Alex Kloss <klossalex@gmail.com>
2011-10-05 00:49:14 -0400

    Add handle_status_update stored procedure to schema
    Eventually this will completely obsolete the Outages table, since it
    has the same information plus the periods of availability. It's possible
    to implement this as a view, but in practice there's no easy way to get
    good performance without materializing the view (which is impossible
    in Postgres) so this is a hack around that. A working, pretty hack, though.

--- schema.sql
+++ schema.sql
@@ -1,12 +1,25 @@
 --- Database schema for print2here_health
 --- Schema developed on PostgreSQL 9.1
 
+---
+--- TABLES
+---
+
 CREATE TABLE outage (
     id serial NOT NULL,
     name character varying(15) NOT NULL,
     start timestamp without time zone NOT NULL,
     "end" timestamp without time zone,
     description character(1)
+);
+
+CREATE TABLE periods (
+    id serial NOT NULL,
+    start timestamp without time zone NOT NULL,
+    "end" timestamp without time zone NOT NULL,
+    name character varying(15) NOT NULL,
+    status character(1) NOT NULL,
+    groupid integer NOT NULL
 );
 
 CREATE TABLE status (
@@ -31,3 +44,38 @@ ALTER TABLE ONLY status
 
 ALTER TABLE ONLY subscription
     ADD CONSTRAINT subscription_pkey PRIMARY KEY (id);
+
+---
+--- FUNCTIONS
+---
+
+CREATE FUNCTION handle_status_update() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    last_group_number INTEGER;
+    last_status character(1);
+BEGIN
+    SELECT groupid, status INTO last_group_number, last_status FROM Periods WHERE name=NEW.name ORDER BY id DESC LIMIT 1;
+
+    IF NOT FOUND THEN
+        INSERT INTO Periods ("start", "end", name, status, groupid) VALUES (NEW.timestamp, NEW.timestamp, NEW.name, NEW.status, 0);
+        RETURN NEW;
+    END IF;
+
+    UPDATE Periods SET "end" = NEW.timestamp WHERE name=NEW.name AND groupid=last_group_number;
+
+    IF last_status <> NEW.status THEN
+        INSERT INTO Periods ("start", "end", name, status, groupid) VALUES (NEW.timestamp, NEW.timestamp, NEW.name, NEW.status, last_group_number + 1);
+        RETURN NEW;
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+---
+--- TRIGGERS
+---
+
+CREATE TRIGGER new_status AFTER INSERT ON status FOR EACH ROW EXECUTE PROCEDURE handle_status_update();
